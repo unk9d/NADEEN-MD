68757c6e2506aa93e0c1f4ad64313c5c768d1eeb
Nadeen Poorna <nadeenpoornaaluth@proton.me>
2025-03-14 21:50:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "YsYlRQaa#heVle58By3R4QweJmw8AdzaAAd8wFawfToUY8mKvfcE", //put your session id
-MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+MONGODB: process.env.MONGODB || "mongodb://mongo:FQQyixqbcbKXBYexczseeuuIqMXgghuo@nozomi.proxy.rlwy.net:57535",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", //true or false
